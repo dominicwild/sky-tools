8d1e266af4368572f7e577782ba8125a84091eb1
dwild8 <dominic55555@hotmail.com>
2025-04-22 20:52:14 +0100

    Use hash key instead of standard JSON object to avoid race conditions

--- server/redis.ts
+++ server/redis.ts
@@ -2,27 +2,25 @@
 
 import Redis from "ioredis"
 import {getSkyDate} from "@/lib/utils";
+import {unstable_noStore as noStore} from 'next/cache';
 
 const client = new Redis(`rediss://default:${process.env.REDIS_TOKEN}@${process.env.REDIS_URL}:6379`);
 
 export type QuestValue = Record<string, number>
 
 export async function getTodaysQuests() {
+    // Opt out of caching at the data fetch level
+    // noStore();
+
     const key = getSkyDate();
-    const json = await client.get(key)
-    if (!json) {
-        return {}
-    }
-    return JSON.parse(json) as QuestValue
+    return client.hgetall(key)
 }
 
 export async function incrementQuest(id: number) {
-    if (process.env.NODE_ENV !== 'production') {
-        return;
-    }
-    const quests = await getTodaysQuests();
-    quests[id] = (quests[id] ?? 0) + 1;
-
+    // if (process.env.NODE_ENV !== 'production') {
+    //     return;
+    // }
+    console.log("increment quest id", id)
     const key = getSkyDate();
-    client.set(key, JSON.stringify(quests));
+    client.hincrby(key, id + "", 1)
 }
